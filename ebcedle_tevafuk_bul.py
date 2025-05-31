b4440e0e02045c1caa1dab3138074fff750c1f53
kurandanbilime <54211306+coding2earn@users.noreply.github.com>
2025-05-31 18:38:34 +0200

    ebcedle_tevafuk_bul.py

--- ebcedle_tevafuk_bul.py
+++ ebcedle_tevafuk_bul.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 
+
 app = Flask(__name__)
 
 ebced_table = {
@@ -41,3 +42,11 @@ def analyze():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
+    import os
+    
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
